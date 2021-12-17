fa917b2ad4c30bffe2504a8a03b0eceff90e221c
david <davidmatovu88@gmail,com>
2021-12-17 23:37:07 +0300

    work on the landing page component

---
+++ vegetable-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LandigPage from './Components/HomePage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LandigPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ vegetable-app/src/Components/HomePage/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './landingPage.css'
+
+const LandigPage =()=>{
+  return (
+    <div className='wrapper'>
+      <div className='navigation'>
+        <div className='nav1'>nav1</div>
+        <div className='nav2'>nav2</div>
+      </div>
+      <div className='showCaseArea'>Flesh Vegetable</div>
+      <div className='support-info'>
+        <div className='free-shipping'>1</div>
+        <div className='support'>2</div>
+        <div className='return'>3</div>
+        <div className='payment'>4</div>
+      </div>
+      <div className='trending'>Trending Products</div>
+    </div>
+  )
+}
+export default LandigPage
